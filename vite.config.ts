4afba80e4d909f1fe5f1bc6bfade0d09a5a4708e
neptune <147700973+emogirls@users.noreply.github.com>
2025-08-13 00:16:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-// https://vitejs.dev/config/
 export default defineConfig(async () => {
   const tailwindcss = (await import('@tailwindcss/vite')).default
 
   return {
+    base: '/',  // Add this line
     plugins: [
       react(),
       tailwindcss()
